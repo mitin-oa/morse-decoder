36441280f47516b3b65876fc0caa697b9cfefcef
mitin_oleg <mitin_oa@mail.ru>
2023-01-18 15:53:20 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -37,8 +37,72 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const Morse_Binary = { '.': '10', '-': '11',' ': '**********' };
+
 function decode(expr) {
-    // write your solution here
+    let decoded_expr = [];
+
+    if (isDecoded(expr)) {
+        let binaryWords = [];
+        split_arr(expr, 10).forEach(word => binaryWords.push(decryptWord(word).join('')));
+        binaryWords.forEach(word => decoded_expr.push(MORSE_TABLE[word] || ' '));
+    } else {
+        expr.split('').forEach((letter) => {
+            const decoded = letter !== ' ' ? getKeyByValue(MORSE_TABLE, letter) : ' ';
+            decoded_expr.push(decoded_symbol(decoded));
+        });
+    }
+
+    return decoded_expr.join('');
+}
+
+function decryptWord(decodedWord) {
+    let decryptedWords = [];
+    if (decodedWord === '**********') { return [' '] }
+    split_arr(decodedWord, 2).forEach(item => {
+        if (item !== '00') {
+            decryptedWords.push(getKeyByValue(Morse_Binary, item))
+        }
+    });
+
+    return decryptedWords;
+}
+
+function decoded_symbol(symb) {
+    if (symb === ' ') {
+        return Morse_Binary[' ']
+    }
+    const decoded = []
+    let templateArray = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
+    symb.split('').forEach((x) => {
+        decoded.push(Morse_Binary[x])
+    })
+
+    const stringToReplace = decoded.join('')
+    templateArray.splice(templateArray.length - stringToReplace.length,
+        stringToReplace.length,
+        stringToReplace);
+    return templateArray.join('');
+}
+
+function split_arr(array, size) {
+    const splited_arr = [];
+    let index = 0;
+    while (index < array.length) {
+        splited_arr.push(array.slice(index, size + index));
+        index += size;
+    }
+
+    return splited_arr;
+}
+
+
+function isDecoded(str) {
+    return RegExp(/^[01*]+$/).test(str);
+}
+
+function getKeyByValue(object ,value) {
+    return Object.keys(object).find(key => object[key] === value);
 }
 
 module.exports = {
